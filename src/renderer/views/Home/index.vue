f526a7ddfdfbb13afe627a888616b762a4d8ba46
liangshaojie <877942720@qq.com>
2019-10-14 15:41:45 +0800

    首页完成了

--- src/renderer/views/Home/index.vue
+++ src/renderer/views/Home/index.vue
@@ -2,24 +2,79 @@
   <home-layout>
     <div class="floors" v-if="isRenderFinish">
       <banner :banners="banners" />
+      <transition-group name="flip-list">
+        <a-card :bordered="false" style="background: transparent;margin-bottom: 20px;" :style="{opacity:isRenderFinish?1:0}" v-for="item in navs" :title="item.name" :key="item.key">
+          <router-link :to="`/${item.key}`" slot="extra" v-if="!item.hideMore">
+            更多
+            <a-icon type="right" />
+          </router-link>
+          <component :is="item.key" :list="data[item.key]" v-if="isRenderFinish" />
+        </a-card>
+      </transition-group>
     </div>
   </home-layout>
 </template>
 
 <script>
 import { getBanner } from '@/api/banner'
+import { getPrivateContent } from '@/api/privatecontent'
+import { getNewSong } from '@/api/song'
+import { getRecommendPlaylist } from '@/api/playlist'
+import { getPersonalizedMV } from '@/api/mv'
+import { getDjHot } from '@/api/dj'
+
 import HomeLayout from '@/layouts/HomeLayout'
 import banner from './components/Banner'
+import privateContent from './components/privateContent'
+import newSong from './components/newSong'
+import playlist from './components/playlist'
+import mv from './components/mv'
+import dj from './components/dj'
 export default {
   data() {
     return {
       banners: [],
-      isRenderFinish: true
+      isRenderFinish: true,
+      data: {
+        privateContent: [],
+        newSong: [],
+        playlist: [],
+        mv: [],
+        dj: []
+      },
+      navs: [
+        {
+          name: '独家放送',
+          key: 'privateContent',
+          hideMore: true
+        },
+        {
+          name: '最新音乐',
+          key: 'newSong'
+        },
+        {
+          name: '推荐歌单',
+          key: 'playlist'
+        },
+        {
+          name: '推荐MV',
+          key: 'mv'
+        },
+        {
+          name: '主播电台',
+          key: 'dj'
+        }
+      ]
     }
   },
   components: {
     HomeLayout,
-    banner
+    banner,
+    privateContent,
+    newSong,
+    playlist,
+    mv,
+    dj
   },
   created() {
     this._getData()
@@ -27,13 +82,36 @@ export default {
   methods: {
     async _getData() {
       this.isRenderFinish = false
-      Promise.all([getBanner()]).then(([{ banners }]) => {
-        this.isRenderFinish = true
-        banners.forEach(banner => {
-          banner.src = banner.imageUrl
-        })
-        this.banners = banners
-      })
+      Promise.all([
+        getBanner(),
+        getPrivateContent(),
+        getNewSong(),
+        getRecommendPlaylist(),
+        getPersonalizedMV(),
+        getDjHot()
+      ]).then(
+        ([
+          { banners },
+          { result: privateContent },
+          { result: newSong },
+          { result: playlist },
+          { result: mv },
+          { djRadios: dj }
+        ]) => {
+          this.isRenderFinish = true
+          banners.forEach(banner => {
+            banner.src = banner.imageUrl
+          })
+          this.banners = banners
+          this.data = {
+            privateContent,
+            newSong,
+            playlist,
+            mv,
+            dj
+          }
+        }
+      )
     }
   }
 }
